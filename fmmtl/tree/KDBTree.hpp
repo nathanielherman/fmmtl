4d4e2a0f27ecca4cadf093469e3a49723648280b
Styliani Pantela <stylianipantela@college.harvard.edu>
2014-05-10 23:16:16 -0400

    kdbtrees first try to compile

---
+++ fmmtl/tree/KDBTree.hpp
@@ -0,0 +1,270 @@
+#pragma once
+/** @file NDTree
+ * @brief General class representing a {1D,2D,3D,4D}-Tree.
+ */
+
+#include <vector>
+#include <bitset>
+#include <algorithm>
+
+#include <iostream>
+#include <iomanip>
+
+#include <boost/iterator/iterator_adaptor.hpp>
+#include <boost/iterator/permutation_iterator.hpp>
+using boost::iterator_adaptor;
+
+#include "fmmtl/util/Logger.hpp"
+#include "fmmtl/numeric/Vec.hpp"
+#include "BoundingBox.hpp"
+#include "MortonCoder.hpp"
+
+#define CACHE_SZ 4096
+
+template <unsigned DIM>
+struct NDBTree {
+
+typedef Vec<DIM,double> point_type;
+typedef BoundingBox<DIM> bounding_box_type;
+
+
+struct Page {
+    bool isRegionPage;
+    uint8_t splittingDomain;
+    uint16_t pidx;
+    Page *parent;
+};
+
+struct RegionPage : Page {
+    struct region_type {
+      BoundingBox<DIM> box;
+      Page *page;
+    };
+    
+    std::vector<region_type> children;
+    RegionPage() : isRegionPage(true), splittingDomain(0), pidx(0), parent(NULL), children() {}
+
+};
+
+struct PointPage : Page {
+    std::vector<point_type> points;
+    PointPage() : isRegionPage(false), splittingDomain(0), pidx(0), parent(NULL), points() {}
+};
+
+
+static constexpr unsigned n_crit_region = CACHE_SZ / (sizeof(RegionPage::region_type));
+static constexpr unsigned n_crit_point = CACHE_SZ / (sizeof(point_type));
+
+  unsigned n_crit_region_;
+  unsigned n_crit_point_;
+  MortonCoder<DIM> coder_;
+
+  template <typename PointIter>
+  NDBTree(PointIter first, PointIter last, unsigned ncr_usr = n_crit_region, unsigned ncp_usr = n_crit_point)
+      : coder_(get_boundingbox(first, last)), n_crit_region_(ncr_usr), n_crit_point_(ncp_usr) {
+
+    // create empty root page
+    pointPages.emplace_back();
+    root = &pointPages[0];
+    
+    insert_range(first, last, n_crit_point_usr, n_crit_point_region_usr);
+  }
+  
+   void print() {
+      for (auto&& p: pointPages) {
+        std::cout << "Point Page \n";
+        for (auto&& point: p.points) {
+          std::cout << point << ", ";
+        }
+        std::cout << std::endl;
+      }
+  }
+  
+  
+  
+  
+  private:
+  
+
+  //! Uses incremental bucket sorting
+  template <typename PointIter>
+  void insert_range(PointIter p_first, PointIter p_last) {
+    
+    for (auto it = p_first; it != p_last; ++it) {
+      if (!insert(*it)) {
+        // inserted duplicate point // TODO: notify user
+        assert(0);
+      }
+    }
+  }
+  
+  // insert a single point
+  bool insert(point_type p) {
+    // we create the root in the constructor
+    assert(root != NULL);
+    
+    PointPage page;
+    if (query(p, page)) {
+      return false;
+    }
+    
+    page.points.push_back(p);
+    
+    if (page.points.size() <= n_crit_point_) {
+      return true;
+    }
+    
+    split(page);
+    
+    return true;
+  }
+
+   void split_rp(RegionPage& p,  Page *right_parent = p.parent) {     
+     auto dim = p.splittingDomain;
+     auto& regions = p.children;
+     auto median = regions.begin() + regions.size()/2;
+     std::nth_element(regions.begin(), median, regions.end(), 
+     [] (region_type r1, region_type r2) { return r1.box.min()[dim] < r2.box.min()[dim]; });
+     
+     auto split_pt = (*median).box.min()[dim];
+     
+     regionPages.emplace_back();
+     RegionPage& right_page = regionPages.back();
+     
+     p.splittingDomain = right_page.splittingDomain = (p.splittingDomain + 1) % DIM;
+
+     // save old (current) regions so we can look through them all
+     std::vector<region_type> old_left_regions = p.children;
+     // clear the vector so we can add only regions that aren't going into right_page
+     p.children = std::vector<region_type>();
+     
+     auto cur_box = p.parent->children[p.pidx].box;
+     point_type left_max = cur_box.max();
+     // box ends at split_pt in the dim dimension
+     left_max[dim] = split_pt;
+     point_type right_min = cur_box.min();
+     // box begins at split_pt in the dim dimension
+     right_min[dim] = split_pt;
+     BoundingBox<DIM> left_box(cur_box.min(), left_max);
+     BoundingBox<DIM> right_box(right_min, cur_box.max());
+     
+     update_parent(region_type{left_box, &p});
+     
+     for (auto&& region : old_left_regions) {
+       if (left_box.contains(region.box)/*region.box.max()[dim] <= split_pt*/) { // fully left of split
+         p.children.push_back(region);
+       } else if (right_box.contains(region.box)/*region.box.min()[dim] >= split_pt*/) { // fully right of split
+         right_page.children.push_back(region);
+       } else { // middle of split
+         //TODO: refactor split to take dim and splitpt
+         split(region.page, &right_page, dim, split_pt);
+       }
+     }
+     p.parent->children[p.pidx].box = left_box;
+     push_page(region_type{right_box, &right_page}, p->parent);
+   }
+
+
+   void split_pp(PointPage p,  Page *right_parent = p.parent) {
+    // median of key_i
+    std::nth_element (p.points.begin(), p.points.end(), 
+      [] (point_type p1, point_type p2) { 
+        return p1[p.splittingDomain] < p2[p.splittingDomain];
+      });
+    point_type median = p.points[p.points.size() / 2];
+
+    // create new right page, the old page will be left
+    pointPages_.emplace_back();
+    PointPage &new_p = pointPages.back();
+    // new points for p
+    std::vector<point_type> ppoints;
+
+    // init new region_types
+    RegionPage::region_type rt_p = {.box = BoundingBox(), .page = p};
+    RegionPage::region_type rt_np = {.box = BoundingBox(), .page = &new_page};
+
+    // reassign the points of the original region
+    for (auto&& pnt: p.points) {
+      if (pnt[p.splittingDomain] < median) {
+        ppoints.push_back(pnt);
+        rt_p.box |= pnt; //
+      }
+      else {
+        new_p.points.push_back(pnt);
+        rt_np.box |= pnt;
+      }
+    }
+    // update p's points and the splittingDomain
+    p.points = ppoints;
+    p.splittingDomain = (p.splittingDomain + 1) % n_crit_point_;
+    new_p.splittingDomain = p.splittingDomain;
+    
+    // update parent to hold new bounding_box
+    update_parent(rt_p);
+    return push_page(p->parent, rt_np);
+   } 
+   
+   void update_parent(region_type &rt) {
+      if (rt.p->parent != NULL) {
+         p.parent->children[p.pid].box = rt.box;
+      }
+      else { // a parent does not exist so create one
+        regionPages_.emplace_back();
+        RegionPage &parent = regionPages.back();
+        parent.children.push_back(rt);
+        rt.p->parent = parent;
+        rt.p->pid = 0;
+        root = &parent;
+      }
+   }
+        
+   void push_page(RegionPage *parent, region_type &rt) {
+    parent->children.push_back(rt);
+    rt.page->parent = parent;
+    rt.page->pid = parent->children.size() - 1;
+    if (parent->children.size() == n_crit_region_) {
+      split(*parent);
+    }
+   }
+  
+  /* @pre p is a full point page the first time this is called
+   * @pre left parent is p.parent
+   */
+
+  void split(Page &p, Page *right_parent = p.parent) {
+      if (p.isRegionPage)
+        split_rp(p, right_parent);
+      else
+        split_pp(p, right_parent);
+  }
+  
+  /** Returns true iff @a p is in the tree.
+   * @param[out] page will be set to the PointPage that either contains or would contain @a p
+   */
+  bool query(point_type p, PointPage& pointpage, Page *head = root) {
+    // we reached the leaf
+    if (!head.isRegionPage) {
+      pointpage = head;
+      // TODO: we could speed this up slightly if we just ignored/assumed no duplicate points
+      // (by not doing this for loop)
+      for (auto&& point : pointpage.points) {
+          if (point == p) {
+              return true;
+          }
+      }
+      return false;
+    }
+  
+    for (auto&& region : head->children) {
+      if (region.box.contains(p)) {
+        return query(p, pointpage, region.page);
+      }
+    }
+  }
+  
+
+  Page *root;
+  
+  std::vector<PointPage> pointPages;
+  std::vector<RegionPage> regionPages;
+};
---
+++ unit_tests/test_kdbtree.cpp
@@ -0,0 +1,25 @@
+#include "fmmtl/tree/KDBTree.hpp"
+#include "fmmtl/numeric/Vec.hpp"
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " NUM_POINTS\n";
+    exit(1);
+  }
+
+  int N = atoi(argv[1]);
+
+  typedef Vec<3,double> point_type;
+
+  std::vector<point_type> points(N);
+  for (int k = 0; k < N; ++k)
+    points[k] = fmmtl::random<point_type>::get();
+
+  NDBTree<3> tree(points.begin(), points.end());
+
+  tree.print();
+}
